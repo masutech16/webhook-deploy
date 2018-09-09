fa65269d859212f06bae82894cb1702dc59a45a6
masutech16 <leafclover64@gmail.com>
2018-09-09 18:00:53 +0900

    add main

---
+++ main.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const {spawn, execSync} = require('child_process')
+
+const createHandler = require('github-webhook-handler')
+const handler = createHandler({path: '/', secret: process.env.SECRET || 'test'})
+
+const server = http.createServer((req, res) => {
+  handler(req, res, err => {
+    res.end('Not found')
+  })
+}).listen(7777)
+
+handler.on('push', event => {
+  const payload = event.payload
+  const branch = payload.ref.split("/").pop()
+
+  if (branch === 'master') {
+    console.log("Deploying...")
+    execSync('sh deploy.sh', {shell: '/bin/bash'})
+  }
+})
